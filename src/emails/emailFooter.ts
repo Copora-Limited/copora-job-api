0cb9ab4a687666441c91c5880c5e97d3a02696f4
Shafi Akinropo <sakinropo@gmail.com>
2024-10-13 12:05:02 +0100

    update with reminder endpoint

--- src/emails/emailFooter.ts
+++ src/emails/emailFooter.ts
@@ -1,56 +1,56 @@
 // emailFooter.js
 export function emailFooter() {
-  return `
-              </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
+    return `
+                </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
 
-            <!-- END MAIN CONTENT AREA -->
-            <tr>
-              <td>
-                <footer style="
-                    background-color: #EAF0F3;
-                    margin-top: -3px;
-                    border-top: 0.5px solid #ddd;
-                  ">
-                 
-                  <div class="paddingMobile" style="
-                      width: 50%;
-                      padding-top: 2rem;
-                      padding-bottom: 8px;
-                      border-collapse: collapse;
-                      text-align: center;
-                      color: #ffffff;
-                      display: flex;
-                      justify-content: space-around;
-                      align-items: center;
-                      margin-right: auto;
-                      margin-left: auto;
+              <!-- END MAIN CONTENT AREA -->
+              <tr>
+                <td>
+                  <footer style="
+                      background-color: #EAF0F3;
+                      margin-top: -3px;
+                      border-top: 0.5px solid #ddd;
                     ">
-                    <a href="https://instagram.com/use_nidi?igshid=MzRlODBiNWFlZA==" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681342/COPORA_IG_oc5r7i.png"></a>
-                    <a href="https://x.com/usenidi?s=21&t=DJhnfOr8u2WipeCDcSFy9A" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681342/COPORA_X_gkddqh.png"></a>
-                    <a href="https://www.linkedin.com/company/usenidi/" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681653/COPORA_IN_mw4ldq_p6kjvr.png"></a>
-                    <a href="https://www.linkedin.com/company/usenidi/" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681653/COPORA_YT_x4omls_y1mwgb.png"></a>
-                  </div>
-                </footer>
-              </td>
-            </tr>
-          </table>
-          <!-- END CENTERED WHITE CONTAINER -->
+                   
+                    <div class="paddingMobile" style="
+                        width: 50%;
+                        padding-top: 2rem;
+                        padding-bottom: 8px;
+                        border-collapse: collapse;
+                        text-align: center;
+                        color: #ffffff;
+                        display: flex;
+                        justify-content: space-around;
+                        align-items: center;
+                        margin-right: auto;
+                        margin-left: auto;
+                      ">
+                      <a href="https://instagram.com/use_nidi?igshid=MzRlODBiNWFlZA==" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681342/COPORA_IG_oc5r7i.png"></a>
+                      <a href="https://x.com/usenidi?s=21&t=DJhnfOr8u2WipeCDcSFy9A" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681342/COPORA_X_gkddqh.png"></a>
+                      <a href="https://www.linkedin.com/company/usenidi/" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681653/COPORA_IN_mw4ldq_p6kjvr.png"></a>
+                      <a href="https://www.linkedin.com/company/usenidi/" style="color: #ffffff"><img src="https://res.cloudinary.com/dhcikhvpu/image/upload/v1728681653/COPORA_YT_x4omls_y1mwgb.png"></a>
+                    </div>
+                  </footer>
+                </td>
+              </tr>
+            </table>
+            <!-- END CENTERED WHITE CONTAINER -->
 
-          <!-- START FOOTER -->
+            <!-- START FOOTER -->
 
-          <!-- END FOOTER -->
-        </div>
-      </td>
-      <td style="font-family: Inter; font-size: 14px; vertical-align: top" valign="top">
-        &nbsp;
-      </td>
-    </tr>
-  </table>
-</body>
+            <!-- END FOOTER -->
+          </div>
+        </td>
+        <td style="font-family: Inter; font-size: 14px; vertical-align: top" valign="top">
+          &nbsp;
+        </td>
+      </tr>
+    </table>
+  </body>
 </html>
-  `;
+    `;
 }
