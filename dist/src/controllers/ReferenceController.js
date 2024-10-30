db62ff832a1e19646630ecf745e80209884c409c
Shafi Akinropo <sakinropo@gmail.com>
2024-10-30 10:20:29 +0100

    update

--- dist/src/controllers/ReferenceController.js
+++ dist/src/controllers/ReferenceController.js
@@ -75,9 +75,9 @@ class ReferenceController {
                             return res.status(400).json({ statusCode: 400, message: 'End Date is required' });
                         }
                         // Ensure startDate is not greater than endDate
-                        if (new Date(startDate) > new Date(endDate)) {
-                            return res.status(400).json({ statusCode: 400, message: 'Start Date cannot be later than End Date' });
-                        }
+                        // if (new Date(startDate) > new Date(endDate)) {
+                        //     return res.status(400).json({ statusCode: 400, message: 'Start Date cannot be later than End Date' });
+                        // }
                         // Check for existing reference by phone
                         const existingReference = yield ReferenceService_1.ReferenceService.findByApplicationNoAndPhone(applicationNo, phone);
                         if (existingReference) {
--- src/controllers/ReferenceController.ts
+++ src/controllers/ReferenceController.ts
@@ -70,9 +70,9 @@ export class ReferenceController {
                     }
 
                     // Ensure startDate is not greater than endDate
-                    if (new Date(startDate) > new Date(endDate)) {
-                        return res.status(400).json({ statusCode: 400, message: 'Start Date cannot be later than End Date' });
-                    }
+                    // if (new Date(startDate) > new Date(endDate)) {
+                    //     return res.status(400).json({ statusCode: 400, message: 'Start Date cannot be later than End Date' });
+                    // }
                     // Check for existing reference by phone
                     const existingReference = await ReferenceService.findByApplicationNoAndPhone(applicationNo, phone);
 
